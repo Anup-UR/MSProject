4640ef8b101613958cc922acd52bc9d1459d1260
Anup-UR <anup_ur7869@hotmail.com>
2022-06-21 23:42:10 +0530

    First Commit

---
+++ Interview.java
@@ -0,0 +1,20 @@
+
+import java.util.HashMap;
+
+public class Interview {
+    public static void main(String[] args) {
+     String name="Testing Character Countaaaa";
+     name=name.toLowerCase();
+        HashMap<Character,Integer> map=new HashMap<>();
+        for (int i=name.length()-1;i>=0;i--) {
+            if (name.charAt(i)!=' ') {
+                if (map.containsKey(name.charAt(i))) {
+                    int count = map.get(name.charAt(i));
+                    map.put(name.charAt(i), ++count);
+                } else
+                    map.put(name.charAt(i), 1);
+            }
+        }
+        System.out.println(map);
+    }
+}
